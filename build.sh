3f771a6c6cfcd69926b035cfad9d82e427b12d38
Griffin <bluedogerino@gmail.com>
2019-07-19 22:21:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
     
 #!/bin/bash
 curl -sLo upload-github-release-asset.sh https://gist.githubusercontent.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447/raw/dbadd7d310ce8446de89c4ffdf1db0b400d0f6c3/upload-github-release-asset.sh
-sudo apt-get install wget curl -y
+apt-get install wget curl -y
 
 if [ -f /home/ci/*.txt ]
 then
